cdc62d6f8e3ca15dd6820cd3e0ebe6a4313e79d7
Alevish <ludepihova@gmail.com>
2022-10-26 19:26:58 +0300

    finish project

---
+++ script.js
@@ -0,0 +1,24 @@
+const dino = document.getElementById('cat');
+const cactus = document.getElementById('cactus');
+
+document.addEventListener("keydown", function(event) {
+    jump();
+});
+
+function jump () {
+    if (cat.classList != "jump") {
+        cat.classList.add("jump")
+    }
+    setTimeout ( function() {
+        dino.classList.remove("jump")
+    }, 700)
+}
+
+let isAlive = setInterval ( function() {
+    let catTop = parseInt(window.getComputedStyle(cat).getPropertyValue("top"))
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+    if (cactusLeft < 50 && cactusLeft > 0 && catTop >= 140) {
+        alert("U R loshara")
+    }
+}, 10)
